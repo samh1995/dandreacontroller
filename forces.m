53119ee3b5681c63d95f35edfa6471cbe53f1929
SamToufaili <samtoufaili@gmail.com>
2017-08-10 11:28:55 -0400

    final code with update graphs/working great!

--- forces.m
+++ forces.m
@@ -7,7 +7,7 @@ initequil;
 
    
     G=[1 1 1; -1 0 1; 0 1 0];
-     H=[(fbar(1)+fbar(2)+fbar(3)); u(1)-fbar(1)+fbar(3);u(2)+fbar(2)];
+     H=[(f_total); u(1)-fbar(1)+fbar(3);u(2)+fbar(2)];
 %     H=[(fbar(1)+fbar(2)+fbar(3)); u(1)-fbar(1)+fbar(3);x(2)];
 %     [1 1 1; -1 0 1; 0 1 0]*[ x(1) x(2) x(3)]'=[(fbar(1)+fbar(2)+fbar(3)); u(1)-fbar(1)+fbar(3);u(2)+fbar(2)];
 %     f(1)=u(1)-x(3)+fbar(3)+x(1)-fbar(1);
--- wholemain.m
+++ wholemain.m
@@ -7,11 +7,12 @@ initequil;
     global  wbar fbar wbbar nbar K
     
   %% Initial Conditions
-    state=[0.133899759 0 0 0 2.4579 18.2494 0 0 5 0 0 0 ]';
-    f=fbar;
-    desiredstate=[0.133899759 0 0 0 2.4579 18.2494 0 1 6 0 0 0 ]';
+%     state=[0.133899759 0 0 0 2.4579 18.2494 0 0 5 0 0 0 ]';
+ state=[0 0 0 0 0 0 0 0 1 0 0 0 ]';
+    f=[1.25 1.25 1.25 0]';
+    desiredstate=[0.133899759 0 0 0 0 0 4 5 8 0 0 0 ]';
 
-    endTime = 10;  % seconds
+    endTime = 30;  % seconds
     dt = 1 / 200; % time step (Hz)
     t=0;
     
@@ -58,20 +59,21 @@ t = t1ODE(end,:)- dt;
     Hist = updatehist(Hist, t, state, f);
    
     W=W+1
-  
+   
    %y(i,:)=y(i,:)-transpose(initialconditions); 
   
 end
 %%
+set(0,'DefaultFigureWindowStyle','docked')
 figure(1)
     plot(Hist.times,Hist.states(1,:)); hold on
 
-      plot(Hist.times,0.133899759*ones(size(Hist.times)));
+      plot(Hist.times,desiredstate(1)*ones(size(Hist.times)));
 %      plot([0 10],[0.133899759 0.133899759]);
-%     title('Roll value vs time (rads vs secs)')
-%     xlabel('t(s)')
-%     ylabel('Roll(rads)')
-    %saveas(figure (1),'state1.jpg')
+  title('Roll value vs time (rads vs secs)')
+  xlabel('t(s)')
+ ylabel('Roll(rads)')
+    saveas(figure (1),'state1.jpg')
 figure(2)
     plot(Hist.times,Hist.states(2,:)); hold on
 
@@ -79,63 +81,63 @@ figure(2)
     title('pitch value vs time (rads vs secs)')
     xlabel('t(s)')
     ylabel('Pitch(rads)')
-    %saveas(figure (2),'state2.jpg')
+    saveas(figure (2),'state2.jpg')
 figure(3)
     plot(Hist.times,Hist.states(3,:))
     title('Yaw value vs time (rads vs secs)') 
     xlabel('t(s)')
     ylabel('Yaw(rads)')
-    %saveas(figure (3),'state3.jpg')
+    saveas(figure (3),'state3.jpg')
     
 figure(4)
     plot(Hist.times,Hist.states(4,:)); hold on
-      plot(Hist.times,0*ones(size(Hist.times)));
+      plot(Hist.times,wbbar(1)*ones(size(Hist.times)));
     title('Roll rate vs time (rads/s vs secs)')
     xlabel('t(s)')
     ylabel('p (rads/s)')
-    %saveas(figure (4),'state4.jpg')
+    saveas(figure (4),'state4.jpg')
     
 figure(5)
     plot(Hist.times,Hist.states(5,:));
     hold on
-    plot(Hist.times,2.4579*ones(size(Hist.times)));
+    plot(Hist.times,wbbar(2)*ones(size(Hist.times)));
     title('pitch rate vs time (rads/s vs secs)')
     xlabel('t(s)')
     ylabel('q (rads/s)')
-    %saveas(figure (5),'state5.jpg')
+    saveas(figure (5),'state5.jpg')
   
 figure(6)
     plot(Hist.times,Hist.states(6,:));
     hold on
-    plot(Hist.times,18.2494*ones(size(Hist.times)));
+    plot(Hist.times,wbbar(3)*ones(size(Hist.times)));
     title('Yaw rate vs time (rads/s vs secs)')
     xlabel('t(s)')
     ylabel('r (rads/s)')
-   % saveas(figure (6),'state6.jpg')
+   saveas(figure (6),'state6.jpg')
 figure(7)
     plot(Hist.times,Hist.states(7,:));
     hold on
-    plot(Hist.times,0*ones(size(Hist.times)));
+    plot(Hist.times,desiredstate(7)*ones(size(Hist.times)));
     title('x position vs time (m vs secs)')
     xlabel('t(s)')
     ylabel('x (m)')
-  %  saveas(figure (7),'state7.jpg')
+  saveas(figure (7),'state7.jpg')
 figure(8)
     plot(Hist.times,Hist.states(8,:));
     hold on
-    plot(Hist.times,0*ones(size(Hist.times)));
+    plot(Hist.times,desiredstate(8)*ones(size(Hist.times)));
     title('y position vs time (m vs secs')
     xlabel('t(s)')
     ylabel('y(m)')
- %   saveas(figure (8),'state8.jpg')
+ saveas(figure (8),'state8.jpg')
 figure(9)
     plot(Hist.times,Hist.states(9,:));
     hold on
-    plot(Hist.times,5*ones(size(Hist.times)));
+    plot(Hist.times,desiredstate(9)*ones(size(Hist.times)));
     title('z position vs time (m vs secs')
     xlabel('t(s)')
     ylabel('z(m)')
-%    saveas(figure (9),'state9.jpg')
+   saveas(figure (9),'state9.jpg')
 
 figure(10)
     plot(Hist.times,Hist.states(10,:));
@@ -144,7 +146,7 @@ figure(10)
     title('x rate vs time (m/s vs secs)')
     xlabel('t(s)')
     ylabel('x dot (m/s)')
-    %saveas(figure (10),'state10.jpg')
+    saveas(figure (10),'state10.jpg')
 figure(11)
     plot(Hist.times,Hist.states(11,:));
     hold on
@@ -152,7 +154,7 @@ figure(11)
     title('y  rate vs time (m/s vs secs)')
     xlabel('t(s)')
     ylabel('y dot (m/s)')
-    %saveas(figure (11),'state11.jpg')
+    saveas(figure (11),'state11.jpg')
 figure(12)
     plot(Hist.times,Hist.states(12,:));
     hold on
@@ -160,7 +162,7 @@ figure(12)
     title('z rate vs time (m/s vs secs)')
     xlabel('t(s)')
     ylabel('z dot (m/s)')
-   % saveas(figure (12),'state12.jpg')
+   saveas(figure (12),'state12.jpg')
 
 figure(13)
   plot(Hist.times,Hist.f(1,:));hold on;
@@ -170,19 +172,18 @@ figure(13)
   xlabel('t(s)')
   ylabel('fi(N)')
   title('4 propeller forces vs time ')
-  %saveas(figure (13),'Propellers.jpg')
+  saveas(figure (13),'Propellers.jpg')
 %%
 figure(14)
     plot(Hist.times,Hist.states(4,:)); hold on
-    plot(Hist.times,0*ones(size(Hist.times))); hold on  
     plot(Hist.times,Hist.states(5,:)); hold on
-    plot(Hist.times,2.4579*ones(size(Hist.times))); hold on
-    plot(Hist.times,Hist.states(6,:)); hold on
-    plot(Hist.times,18.2494*ones(size(Hist.times)));
+    
+    plot(Hist.times,Hist.states(6,:));
+
     title('omega (pqr) vs time (rads/s vs secs)')
     xlabel('t(s)')
     ylabel('r (rads/s)')
-   % saveas(figure (6),'state6.jpg')
+   saveas(figure (14),'pqr.jpg')
  
  %% Visualize simulation.
 %simvisualization(Hist.times, Hist.states, 'V1');
